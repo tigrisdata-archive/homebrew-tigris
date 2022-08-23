f329a4faaa44eb79d9ed8d65a8a98028c0216aa7
Evgeniy Firsov <firs1978@gmail.com>
2022-08-23 12:55:02 -0700

    Initial

---
+++ Formula/tigris-cli.rb
@@ -0,0 +1,21 @@
+class TigrisCli < Formula
+  desc "Tigris is a modern, scalable backend for building real-time websites and apps."
+  homepage "https://docs.tigrisdata.com"
+  license "Apache 2.0"
+  head "https://github.com/tigrisdata/tigris-cli.git", branch: "alpha"
+  url "https://github.com/tigrisdata/tigris-cli.git",
+    tag:      "v1.0.0-alpha.15"
+
+  depends_on "make" => :build
+  depends_on "go" => :build
+  depends_on "docker"
+
+  def install
+    system "make"
+    bin.install "tigris"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Tigris command line tool
+
+## How do I install these formulae?
+
+Run `brew install tigrisdata/tigris/tigris-cli`
+
+Or `brew tap tigrisdata/tigris` and then `brew install tigris-cli`.
+
+## Documentation
+
+Check [Tigris documentation](https://docs.tigrisdata.com).
