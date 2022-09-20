02faf9c3a592b26f6eb0af514bea2fd04a03673c
Yevgeniy Firsov <firsov@tigrisdata.com>
2022-09-19 22:06:19 -0700

    feat: Update to CLI v1.0.0-alpha.26

--- Formula/tigris-cli.rb
+++ Formula/tigris-cli.rb
@@ -4,7 +4,7 @@ class TigrisCli < Formula
   license "Apache 2.0"
   head "https://github.com/tigrisdata/tigris-cli.git", branch: "alpha"
   url "https://github.com/tigrisdata/tigris-cli.git",
-    tag:      "v1.0.0-alpha.18"
+    tag:      "v1.0.0-alpha.26"
 
   depends_on "make" => :build
   depends_on "go" => :build
